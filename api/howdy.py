c76a98d0f61ab16628fe297aa6303d9c7163b89c
Razan-abdullah <razanaalabdallah017@gmail.com>
2022-11-02 11:21:31 +0000

    d

--- api/howdy.py
+++ api/howdy.py
@@ -1,17 +1,12 @@
-
 from http.server import BaseHTTPRequestHandler
-from urllib import parse 
-
 
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
-       
-
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-     
-        self.wfile.write()
+        print("hello Razan :)")
+        message="welcome howdy"
+        self.wfile.write(message.encode())
         return
